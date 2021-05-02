e36515f54bffc800eadfe058e4651a563e993e40
esantiagooo <77802371+esantiagooo@users.noreply.github.com>
2021-05-01 21:15:09 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue > 2500) {
+    return "No can do."
+  } else if (someValue > 2000) {
+    return "I will gladly take your thirty bucks."
+  } else if (someValue => 400){
+    return "This one is on me!"
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let answer
+  (city === "NYC") ? (answer = "Ok, sounds good.") : (answer = "No go.");
+  return answer
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(money){
+  let answer
+  switch(money) {
+    case 'generous':
+      answer = "Thank you so much."
+      break;
+      case "not as generous":
+        answer = "Thank you."
+        break;
+        default:
+        answer = "Bye." 
+        break;
+  }
+  return answer
   // Write your code here!
+
+  
 }
